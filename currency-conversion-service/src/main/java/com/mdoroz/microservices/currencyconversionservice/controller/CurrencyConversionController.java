d0f318a7a994ba3e82d822970335446034fe1bbb
Michał Doroz <michaldoroz7@gmail.com>
2023-07-15 21:20:46 +0200

    Added Feign proxy

--- currency-conversion-service/src/main/java/com/mdoroz/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/mdoroz/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,6 +2,8 @@ package com.mdoroz.microservices.currencyconversionservice.controller;
 
 
 import com.mdoroz.microservices.currencyconversionservice.bean.CurrencyConversion;
+import com.mdoroz.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,9 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
@@ -27,6 +32,20 @@ public class CurrencyConversionController {
         CurrencyConversion currencyConversion = responseEntity.getBody();
 
         assert currencyConversion != null;
+        return new CurrencyConversion(currencyConversion.getId(),
+                from,
+                to,
+                currencyConversion.getConversionMultiple(),
+                quantity,
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment());
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        CurrencyConversion currencyConversion = proxy.getExchangeValue(from, to);
+
         return new CurrencyConversion(currencyConversion.getId(),
                 from,
                 to,
---
+++ currency-conversion-service/src/main/java/com/mdoroz/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.mdoroz.microservices.currencyconversionservice.proxy;
+
+
+import com.mdoroz.microservices.currencyconversionservice.bean.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversion getExchangeValue(@PathVariable String from, @PathVariable String to);
+
+    }
+
